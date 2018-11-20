8acdbe146fbfb74d0a7ed4bcba9447486372ec66
UniquelyHougang <45200553+UniquelyHougang@users.noreply.github.com>
2018-11-20 21:21:09 +0800

    Add files via upload

---
+++ docs/data/JuniorCollege_3.js
@@ -0,0 +1,8 @@
+var json_JuniorCollege_3 = {
+"type": "FeatureCollection",
+"name": "JuniorCollege_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "PIO_NAME": "SERANGOON JUNIOR COLLEGE", "POI_ADDRES": "1033 UPPER SERANGOON ROAD", "POSTCODE": "534768", "POI_CHAR": "Junior Colleges\/Centralised Institute", "POI_ATTRI1": "NORTH 4", "POI_YEAR": "2016", "POI_TYPE": "Education", "XCOORD": 33971.1516, "YCOORD": 38579.5154, "POI_CODE": 1.0 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 103.88697346613074, 1.365173351608618 ] ] } }
+]
+}
